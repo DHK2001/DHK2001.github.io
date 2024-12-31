963063ef0ba97e8c08b088d402f5fc61ce8ae8f8
DHK2001 <64647310+DHK2001@users.noreply.github.com>
2024-12-31 16:00:17 -0600

    nuevas secciones

---
+++ scripts/test-scripts.js
@@ -0,0 +1,5 @@
+const nowhitespace = document.getElementById('noWhitespace').innerHTML;
+console.log(nowhitespace);
+
+const whitespace = document.getElementById('whitespace').innerHTML;
+console.log(whitespace);
