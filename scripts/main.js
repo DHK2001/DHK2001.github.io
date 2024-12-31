f02712cce694f4714db59d35be49522f2075e5ee
DHK2001 <64647310+DHK2001@users.noreply.github.com>
2024-12-31 16:17:09 -0600

    correcciones

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,5 @@
-const myImage = document.querySelector('img');
+const nowhitespace = document.getElementById('noWhitespace').innerHTML;
+console.log(nowhitespace);
 
-myImage.addEventListener('click', () => {
-    const mySrc = myImage.getAttribute('src');
-    if (mySrc === 'img/mozilla.png') {
-        myImage.setAttribute('src', 'img/mozillafirefox.png');
-    } else {
-        myImage.setAttribute('src', 'img/mozilla.png');
-    }
-});
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    const storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla, ' + storedName;
-}
-
-myButton.addEventListener('click', () => {
-    setUserName();
-});
-
-function setUserName() { 
-    const myName = prompt('Please enter your name.');
-    if (!myName) {
-        alert('Please enter a valid name.');
-        setUserName();
-    } else {
-        localStorage.setItem('name', myName);
-        myHeading.textContent = 'Mozilla, ' + myName;
-    }
-}
+const whitespace = document.getElementById('whitespace').innerHTML;
+console.log(whitespace);
--- scripts/test-scripts.js
+++
@@ -1,5 +0,0 @@
-const nowhitespace = document.getElementById('noWhitespace').innerHTML;
-console.log(nowhitespace);
-
-const whitespace = document.getElementById('whitespace').innerHTML;
-console.log(whitespace);
